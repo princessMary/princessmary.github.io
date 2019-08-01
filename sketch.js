2b5f8201cad8d41f72cfc718a01405d08cafed1b
princessMary <43845187+princessMary@users.noreply.github.com>
2019-08-01 10:53:49 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,10 @@ var x = 300;
 var y = 300;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(255);
+	createCanvas(400, 400);
+	ellipse(50, 50, 20, 20);
 }
 
 function draw() {
-	ellipse(mouseX, mouseY, 20, 20);
+	
 }
