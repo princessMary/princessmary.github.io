86695a17cfcda869a31aa792f6fbe00b12926646
princessMary <43845187+princessMary@users.noreply.github.com>
2019-08-21 20:06:40 +0300

    sketch file

---
+++ sketch4.js
@@ -0,0 +1,68 @@
+var x = 300;
+var y = 300;
+
+function handleClick(sketch) {
+  window.selectedSketch = sketch;
+}
+window.selectedSketch = 'sketch1';
+
+function drawSketch1() {
+  if (mouseIsPressed) {
+    var rar = random(-40, 40);
+    var ran = random(-50, 50);
+    noStroke();
+    fill(x / 2, y / 2, random(50, 255), random(0, 255));
+    ellipse(mouseX + rar, mouseY + ran, 20, 20);
+    x = mouseX;
+    y = mouseY;
+    x = x + random(-30, 30);
+    y = y + random(-30, 30);
+
+    if (x > width) {
+      x = x - 100;
+    } else if (x < 0) {
+      x = random(100, 300);
+    } else if (y > height) {
+      y = y - 100;
+    } else if (y < 0) {
+      y = random(10, 100);
+    }
+  }
+}
+
+function drawSketch2() {
+  if (mouseIsPressed){
+    var rar = random(-40,40);
+    var ran = random(-50,50);
+   noStroke();
+   fill(x/2,y/2,random(50,255), random(0,255));
+   ellipse(mouseX+rar, mouseY+ran, 200, 200);
+    x = mouseX
+    y = mouseY
+   x = x + random(-130,130);
+   y = y + random(-130,130);
+
+   if (x>width){
+    x = x-100;
+   } else if(x<0){
+    x = random(100,300);
+   } else if(y > height){
+     y = y-100;
+   } else if (y< 0){
+    y = random(10,100);
+   }
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight - 100);
+  background(255);
+}
+
+function draw() {
+ if (window.selectedSketch === 'sketch1') {
+   drawSketch1();
+ } else {
+      drawSketch2();
+    }
+}
