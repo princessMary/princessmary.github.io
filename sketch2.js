7f964909f7be32682370201a4eb310504f93b855
princessMary <43845187+princessMary@users.noreply.github.com>
2019-08-21 09:42:54 +0300

    Add files via upload

---
+++ sketch2.js
@@ -0,0 +1,33 @@
+var x = 300;
+var y = 300;
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight-100);
+	background(255);
+
+}
+
+function draw() {
+  if (mouseIsPressed){
+    var rar = random(-40,40);
+    var ran = random(-50,50);
+  	noStroke();
+  	fill(x/2,y/2,random(50,255), random(0,255));
+  	ellipse(mouseX+rar, mouseY+ran, 200, 200);
+    x = mouseX
+    y = mouseY
+  	x = x + random(-130,130);
+  	y = y + random(-130,130);
+
+  	if (x>width){
+  		x = x-100;
+  	} else if(x<0){
+  		x = random(100,300);
+  	} else if(y > height){
+  	  y = y-100;
+  	} else if (y< 0){
+  		y = random(10,100);
+  	}
+  }
+}
